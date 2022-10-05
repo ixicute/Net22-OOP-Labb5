0ab41ac00b88e7ba001a76090bb96c262e14fe53
ixicute <ixicute@DESKTOP-QPEIFG9>
2022-10-05 15:20:27 +0200

    Add project files.

---
+++ Net22-OOP-CircleClass/Circle.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Net22_OOP_CircleClass
+{
+    internal class Circle
+    {
+        //Fields
+        private const float pi = 3.141f;
+        private int radius;
+
+        //Constructor
+        public Circle(int radius)
+        {
+            this.radius = radius;
+        }
+
+        public float GetArea()
+        {
+            float math,
+                  result;
+
+            math = radius * radius * pi;
+
+            //Explicit type-casting and limiting output to 3 decimals.
+            result = (float)Math.Round(math, 3);
+
+            return result;
+        }
+
+        public float GetVolume()
+        {
+            float math,
+                  result;
+
+            math = (4.0f / 3f) * pi * radius * radius * radius;
+
+            //Explicit type-casting and limiting output to 3 decimals.
+            result = (float)Math.Round(math, 3);
+
+            return result;
+        }
+
+        public float GetCircumference()
+        {
+            float math,
+                  result;
+
+            math = 2 * pi * radius;
+
+            //Explicit type-casting and limiting output to 3 decimals.
+            result = (float)Math.Round(math, 3);
+
+            return result;
+        }
+    }
+}
---
+++ Net22-OOP-CircleClass/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace Net22_OOP_CircleClass
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Making 2 objects that hold different values for the radius.
+            var circle_1 = new Circle(5);
+            var circle_2 = new Circle(6);
+
+            Console.WriteLine("En cirkel med radie 5 ger följande resultat: \n" +
+                              "Arean är: {0}\n" +
+                              "Volymen blir: {1}\n" +
+                              "Omkretsen blir: {2}",
+                              circle_1.GetArea(), circle_1.GetVolume(), circle_1.GetCircumference());
+
+            Console.WriteLine("----------------------------------------------------------");
+
+            Console.WriteLine("En cirkel med radie 6 ger följande resultat: \n" +
+                              "Arean är: {0}\n" +
+                              "Volymen blir: {1}\n" +
+                              "Omkretsen blir: {2}",
+                              circle_2.GetArea(), circle_2.GetVolume(), circle_2.GetCircumference());
+
+        }
+    }
+}
